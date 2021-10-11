d5c1e4d8cd51256354b2e6213723fcf36b4f5ea3
Elias Assaf <elyas51000@gmail.com>
2021-10-11 23:25:25 +0300

    added some helper functions

--- getData.py
+++ getData.py
@@ -8,28 +8,38 @@ import numpy as np
 
 client = Client("GFZ")
 
-t0 = UTCDateTime("2016-02-27T06:45:00.000")
+t0 = UTCDateTime("2014-09-12T00:00:00.0000Z")
 
 stations = [["IS", "MMA*"], ["IS", "MMB*"], ["IS", "MMC*"]] #[networkID, Station Names]
 
-add_4_mins = lambda x: x + 4 * 60
+mins_4 = lambda x: x + 4 * 60
+day_1 = lambda x: x + 24 * 60 * 60
 
-def query_data(network, station, starttime, endtime, level="response"):
-    inv = client.get_stations(network=network, station=station, level=level).get_contents().get('channels')
-    bulk = list(map(lambda x: x.split('.'), inv))
+def query_data(stations, starttime, endtime, level="response"):
+    inv = query_inventories(stations)
+    return client.get_waveforms_bulk(make_bulk(inv, starttime, endtime), attach_response= True), inv
+
+def make_bulk(inv, starttime, endtime):
+    channels = inv.get_contents().get('channels')
+    bulk = list(map(lambda x: x.split('.'), channels))
     list(map(lambda x: x.extend([starttime, endtime]), bulk))
-    return client.get_waveforms_bulk(bulk)
+    return bulk
 
-def wrapper(func, arg_list):
-    return lambda starttime: func(*arg_list, starttime, add_4_mins(starttime))
+def query_inventory(network, station, level = "response"):
+    return client.get_stations(network=network, station=station, level=level)
 
-choose_time = lambda starttime: reduce(lambda x,y: x+y, map(lambda station: wrapper(query_data, station)(starttime), stations))
-stream = choose_time(t0)
+def query_inventories(stations, level = "response"):
+    return reduce(lambda x,y: x+y, map(lambda station: query_inventory(*station, level), stations))
+
+choose_time = lambda starttime, endtime: query_data(stations, starttime, endtime)
+stream, inv = choose_time(t0, mins_4(t0))
+#stream.remove_response(output="VEL")
 matrix = np.empty((len(stream), 9600))
 
-for i,tr in enumerate(stream):
-    matrix[i] =  tr.data[(tr.times(reftime=t0) >= 0)][0:9600]
+# for i,tr in enumerate(stream):
+#     matrix[i] =  tr.data[(tr.times(reftime=t0) >= 0)][0:9600]
+#     # remove_response(output = "VEL")
 
-mdict = {'time': t0}
-mdict['data'] = matrix
-savemat("data.mat", mdict)
+# mdict = {'time': t0}
+# mdict['data'] = matrix
+# savemat("data.mat", mdict)
