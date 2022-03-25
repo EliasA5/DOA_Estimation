6d1f4ae3530dccb32906591fa4a6c98d9a66aa05
malaakKH <malaak@post.bgu.ac.il>
2022-03-25 22:26:06 +0300

    CRB + simulation for MLE and Fisher

--- CRB.m
+++ CRB.m
@@ -1,6 +1,41 @@
-function [crb] = CRB(v_0,phi,r_k,K_3,K_1,theta,omega_m,R_v,M,s)
+function [crb] = CRB(type,v_0,alpha,theta,Rv,M,X,a_f,da_f,P)
 %This function calculates the CRB
-da = A_derivative(v_0,phi,r_k,K_3,K_1,theta,omega_m);
-J = FIM(s,M,R_v,da);
-crb = 1/J;
+% depending on the type of the bound ('cyclic 1', 'cyclic 2','regular')
+Rv_inv = pinv(Rv);
+X_w = squeeze(sum(X,1));
+s = zeros(1,M);
+FIM = 0;
+for j = 1 : M
+    x_i = X_w(:,j);
+    % calculate the a and a derivative 
+    a = a_f(j, theta, alpha, v_0);
+    da = da_f(j, theta, alpha, v_0);
+
+    % estimating s(m) using the MLE of the deterministic signal
+    down = a' * Rv_inv * a;   
+    s_j = a' * Rv_inv * x_i / P;   % per the formula only x is a variable of p
+    s(j) = s_j / down;
+
+    dmue = da * s(j);
+    
+    FIM = FIM + real(dmue' * Rv_inv * dmue);
 end
+
+FIM = FIM * 2;
+
+switch(type)
+    
+    case 'cyclic 1'
+        crb =  FIM ^ (-1);
+        crb = 2 - 2/(sqrt(crb + 1));
+
+    case 'cyclic 2'
+        crb =  FIM ^ (-1);
+        crb = 2 + 2 * (1 - sqrt(1 + 2 * crb))/crb;
+
+    case 'regular'
+        crb = FIM ^ (-1);
+
+end
+
+
--- ML_simulation.m
+++ ML_simulation.m
@@ -5,23 +5,39 @@ clc
 % This scripts performs the comparison between the MLE and Fisher's scoring
 % for white noise and colored noise
 % All signal's are in the frequency domain
-%%
+%--------------------------------------------------------------------------
+%% Initialization
+
+M = 10;                          
+P = 1;
 K_1 = 15;
 K_3 = 1;
+K = 3 * K_3 + K_1;
+
 alpha = pi/4;
-v_0 = 1;  % P waves velocities can be from 6 Km/sec to 11 Km/sec
-sigma_source = 1;
-sigma_noise = 1;  % for white and colored noise
+v_0 = 9600;                     % P waves velocities can be from 6 Km/sec to 11 Km/sec
+f = 3;                        
+w = f * ones(M,1);              % the omega_m is constant at this point for all the frquencies
+
+SNR = 10;
+sigma_source = 10;
+sigma_noise = 1;                % for white and colored noise
+
 data = load("data.mat");
-rm = data.r_m;   % r_m is loaded form the data matrix we extrcted from the getData python script
-M = 10;   % for these simlutaions we decided to use one segement
-P = 1;
-f = 3;      % the omega_m is constant at this point for all the frquencies
-acc = 0.001;   % this is the accuracy for the MLE sweep in method 1
-iters = 100;    % the number of iterations for the Fisher's scoring (second method)
-[a_model, ~] = model(rm, K_1, K_3, f*ones(M,1));
-%%
-Tests = 11;
+rm = data.r_m;                  % r_m is loaded form the data matrix we extrcted from the getData python script
+
+acc = 0.001;                    % this is the accuracy for the MLE sweep in method 1
+iters = 400;                    % the number of iterations for the Fisher's scoring (second method)
+step_size_white = 1;
+step_size_colored = 1;
+gamma = 0.95;
+theta_0 = pi/4;                 % starting estimate at 45 deg
+
+[a_model, da_model] = model(rm, K_1, K_3, w);
+
+%% Estimation 
+
+Tests = 10;
 
 RMSPE_MLE_colored = [];
 CyclicErr_MLE_colored = [];
@@ -39,17 +55,21 @@ RMSPE_fisher_white = [];
 CyclicErr_fisher_white = [];
 ThetaEst_fisher_white = [];
 
-CRB_white = [];
-CRB_colored = [];
-theta_og = -pi +2*pi/Tests : 2*pi/Tests : pi-2*pi/Tests;
-%theta_og = zeros(Tests,1);
+CRB_white_reg = [];
+CRB_colored_reg = [];
+CRB_white_cyc1 = [];
+CRB_colored_cyc1 = [];
+CRB_white_cyc2 = [];
+CRB_colored_cyc2 = [];
 
-parfor i = 1:Tests-1
+theta_og = -pi +2*pi/Tests : 2*pi/Tests : pi;
 
-    theta = theta_og(i); % theta is in [-pi,pi]
-    theta_0 = 0;
-    [X_colored,~,Rv_colored,~] = synData(rm, theta, alpha, v_0, sigma_source, sigma_noise, M, 'colored', f, K_1, K_3, P);
-    [X_white,s,Rv_white,a] = synData(rm, theta, alpha, v_0, sigma_source, sigma_noise, M, 'white', f, K_1, K_3, P);
+parfor i = 1 : Tests
+
+    theta = theta_og(i);         % theta is in [-pi,pi]
+
+    [X_colored,~,Rv_colored,~] = synData(rm, theta, alpha, v_0, sigma_source, sigma_noise, M, 'colored', w, K_1, K_3, P);
+    [X_white,~,Rv_white,~] = synData(rm, theta, alpha, v_0, sigma_source, sigma_noise, M, 'white', w, K_1, K_3, P);
 
 
     % MLE for colored noise
@@ -67,21 +87,31 @@ parfor i = 1:Tests-1
     ThetaEst_MLE_white = [ThetaEst_MLE_white, theta_white_MLE];
 
     % Fisher's scoring for colored noise
-    theta_colored_fisher = real(Fisher_scoring(theta_0,s,Rv_colored,f,v_0,alpha,K_3,K_1,X_colored,iters,rm));
+    [~,theta_colored_fisher] = Fisher_scoring('syn',theta_0,Rv_colored,v_0,alpha,K,X_colored,iters,step_size_colored,gamma,M,P,a_model,da_model);
     RMSPE_fisher_colored = [RMSPE_fisher_colored, MSPE(theta, theta_colored_fisher, 'MSPE')];
     CyclicErr_fisher_colored = [CyclicErr_fisher_colored, MSPE(theta, theta_colored_fisher, 'cyclic')];
     ThetaEst_fisher_colored = [ThetaEst_fisher_colored, theta_colored_fisher];
     
     % Fisher's scoring for white noise
-    theta_white_fisher = real(Fisher_scoring(theta_0,s,Rv_white,f,v_0,alpha,K_3,K_1,X_white,iters,rm));
+    [~,theta_white_fisher] = Fisher_scoring('syn',theta_0,Rv_white,v_0,alpha,K,X_white,iters,step_size_white,gamma,M,P,a_model,da_model);
     RMSPE_fisher_white = [RMSPE_fisher_white, MSPE(theta, theta_white_fisher, 'MSPE')];
     CyclicErr_fisher_white = [CyclicErr_fisher_white, MSPE(theta, theta_white_fisher, 'cyclic')];
     ThetaEst_fisher_white = [ThetaEst_fisher_white, theta_white_fisher];
 
-    % Calculating the CRB
-    CRB_white(i) = CRB(v_0,alpha,rm,K_3,K_1,theta,f,Rv_white,M,s);
-    CRB_colored(i) = CRB(v_0,alpha,rm,K_3,K_1,theta,f,Rv_colored,M,s);
+    % Calculating the CRB (calculating all kinds)
+
+    CRB_white_reg = [CRB_white_reg, CRB('regular',v_0,alpha,theta,Rv_white,M,X_white,a_model,da_model,P)];
+    CRB_colored_reg = [CRB_colored_reg, CRB('regular',v_0,alpha,theta,Rv_colored,M,X_colored,a_model,da_model,P)];
+
+    CRB_white_cyc1 = [CRB_white_cyc1, CRB('cyclic 1',v_0,alpha,theta,Rv_white,M,X_white,a_model,da_model,P)];
+    CRB_colored_cyc1 = [CRB_colored_cyc1, CRB('cyclic 1',v_0,alpha,theta,Rv_colored,M,X_colored,a_model,da_model,P)];
+
+    CRB_white_cyc2 = [CRB_white_cyc2, CRB('cyclic 2',v_0,alpha,theta,Rv_white,M,X_white,a_model,da_model,P)];
+    CRB_colored_cyc2 = [CRB_colored_cyc2, CRB('cyclic 2',v_0,alpha,theta,Rv_colored,M,X_colored,a_model,da_model,P)];
 end
+
+%--------------------------------------------------------------------------
+%% Graphs 
 
 % figure;
 % hold on;
@@ -95,22 +125,65 @@ end
 % grid on;
 % legend('fisher null' , 'fisher colored' , 'MLE null' , 'MLE colored' , 'CRB white' , 'CRB colored');
 
+% figure;
+% hold on;
+% plot(theta_og,RMSPE_fisher_white(1:Tests-1))
+% plot(theta_og,RMSPE_fisher_colored(1:Tests-1))
+% plot(theta_og,RMSPE_MLE_white(1:Tests-1))
+% plot(theta_og,RMSPE_MLE_colored(1:Tests-1))
+% plot(theta_og,CRB_white(1:Tests-1),'*')
+% plot(theta_og,CRB_colored(1:Tests-1),'-o')
+% hold off;
+% grid on;
+% legend('fisher null' , 'fisher colored' , 'MLE null' , 'MLE colored' , 'CRB white' , 'CRB colored');
+
 figure;
-hold on;
-plot(theta_og,RMSPE_fisher_white(1:Tests-1))
-plot(theta_og,RMSPE_fisher_colored(1:Tests-1))
-plot(theta_og,RMSPE_MLE_white(1:Tests-1))
-plot(theta_og,RMSPE_MLE_colored(1:Tests-1))
-plot(theta_og,CRB_white(1:Tests-1),'*')
-plot(theta_og,CRB_colored(1:Tests-1),'-o')
-hold off;
-grid on;
-legend('fisher null' , 'fisher colored' , 'MLE null' , 'MLE colored' , 'CRB white' , 'CRB colored');
+hold on; grid on;
+plot(theta_og,'*','LineWidth', 2)
+plot(ThetaEst_MLE_white,'o'); plot(ThetaEst_MLE_colored,'o');
+plot(ThetaEst_fisher_white,'x'); plot(ThetaEst_fisher_colored,'x');
+legend('Original', 'MLE W', 'MLE C', 'Fisher W', 'Fisher C')
+xlabel('Test'); ylabel('\theta'); title('Comparison of the Estimations')
+hold off
 
 
+
+%% Circle
+angles = linspace(0, 2*pi, 500);
+radius = 20;
+CenterX = 50;
+CenterY = 40;
+x = radius * cos(angles) + CenterX;
+y = radius * sin(angles) + CenterY;
+
+x_og = radius * cos(theta_og) + CenterX;
+x_MLE_white = radius * cos(ThetaEst_MLE_white) + CenterX;
+x_MLE_colored = radius * cos(ThetaEst_MLE_colored) + CenterX;
+x_Fisher_white = radius * cos(ThetaEst_fisher_white) + CenterX;
+x_Fisher_colored = radius * cos(ThetaEst_fisher_colored) + CenterX;
+
+y_og = radius * sin(theta_og) + CenterY;
+y_MLE_white = radius * sin(ThetaEst_MLE_white) + CenterY;
+y_MLE_colored = radius * sin(ThetaEst_MLE_colored) + CenterY;
+y_Fisher_white = radius * sin(ThetaEst_fisher_white) + CenterY;
+y_Fisher_colored = radius * sin(ThetaEst_fisher_colored) + CenterY;
+
+
+figure
+hold on; grid on ; axis equal;
+plot(x, y, 'k-', 'LineWidth', 1);
+plot(x_og, y_og, 'r*','LineWidth', 2.5)
+plot(x_MLE_white, y_MLE_white, 'o','LineWidth', 1.5)
+plot(x_MLE_colored, y_MLE_colored, 'o','LineWidth', 1.5)
+plot(x_Fisher_white, y_Fisher_white, 'x','LineWidth', 1.5)
+plot(x_Fisher_colored, y_Fisher_colored, 'x','LineWidth', 1.5)
+xlabel('X', 'FontSize', 10); ylabel('Y', 'FontSize', 10);
+legend('','Original', 'MLE W', 'MLE C', 'Fisher W', 'Fisher C')
+title('Comparison of the Estimations on Circle')
 
 
 %--------------------------------------------------------------------------
+%% Functions
 function [fun] = toMaximize(a, R, X_w, M, P)
     R_inv = pinv(R);
     X_w_p = squeeze(sum(X_w,1));
