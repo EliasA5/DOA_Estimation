78f77b2d35a36e259d1181948dc657f75656fc07
Elias Assaf <elyas51000@gmail.com>
2022-03-27 19:29:28 +0300

    add MSE error calculation

--- MSPE.m
+++ MSPE.m
@@ -4,4 +4,6 @@ switch(type)
         err = (cos(theta) - cos(theta_est)).^2 + (sin(theta) - sin(theta_est)).^2;
     case 'MSPE'
         err = custom_mod2pi(theta-theta_est).^2;
+    case 'MSE'
+        err = (theta - theta_est).^2;
 end
