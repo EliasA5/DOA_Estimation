2702a7ecc969e6eb61da011bae650e36307d0123
Elias Assaf <elyas51000@gmail.com>
2022-03-30 14:00:24 +0300

    make ml simulation save workspace and add printing script for said saved worksapce

--- ML_simulation.m
+++ ML_simulation.m
@@ -36,7 +36,7 @@ theta_0 = pi/4-1e-1;                 % starting estimate at 45 deg
 
 %% Estimation 
 
-Tests = 36;
+Tests = 102;
 
 RMSPE_MLE_colored_colored_b = [];
 CyclicErr_MLE_colored_colored_b = [];
@@ -70,8 +70,8 @@ CRB_white_cyc2_b = [];
 CRB_colored_cyc2_b = [];
 
 %f = waitbar(0,'Please wait...');
-J = 1;
-for j=1:3
+J = 20;
+for j=1:J
 %waitbar(j/(J+1), f, append('iter: ', string(j), ' of ', string(J)));
 RMSPE_MLE_colored_colored = [];
 CyclicErr_MLE_colored_colored = [];
@@ -252,190 +252,11 @@ CRB_colored_cyc1 = mean(CRB_colored_cyc1_b ,1);
 CRB_white_cyc2 = mean(CRB_white_cyc2_b ,1);
 CRB_colored_cyc2 = mean(CRB_colored_cyc2_b ,1);
 %--------------------------------------------------------------------------
-%% Graphs 
 
-% figure;
-% hold on;
-% plot(theta_og,RMSPE_fisher_white_white)
-% plot(theta_og,RMSPE_fisher_colored_colored)
-% plot(theta_og,RMSPE_MLE_white_white)
-% plot(theta_og,RMSPE_MLE_colored_colored)
-% plot(theta_og,CRB_white)
-% plot(theta_og,CRB_colored)
-% hold off;
-% grid on;
-% legend('fisher null' , 'fisher colored' , 'MLE null' , 'MLE colored' , 'CRB white' , 'CRB colored');
-
-% figure;
-% hold on;
-% plot(theta_og,RMSPE_fisher_white_white(1:Tests-1))
-% plot(theta_og,RMSPE_fisher_colored_colored(1:Tests-1))
-% plot(theta_og,RMSPE_MLE_white_white(1:Tests-1))
-% plot(theta_og,RMSPE_MLE_colored_colored(1:Tests-1))
-% plot(theta_og,CRB_white(1:Tests-1),'*')
-% plot(theta_og,CRB_colored(1:Tests-1),'-o')
-% hold off;
-% grid on;
-% legend('fisher null' , 'fisher colored' , 'MLE null' , 'MLE colored' , 'CRB white' , 'CRB colored');
-
-%% figure to show the convergance of the methods W-W, C-C
-figure;
-hold on; grid on;
-plot(SNR,theta_og*ones(size(SNR)),'-x','LineWidth', 2)
-plot(SNR,ThetaEst_MLE_white_white,'-o'); plot(SNR,ThetaEst_MLE_colored_colored,'-o');
-plot(SNR,ThetaEst_fisher_white_white,'-*'); plot(SNR,ThetaEst_fisher_colored_colored,'-*');
-legend('Original', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
-xlabel('SNR'); ylabel('\theta'); title('Comparison of the Estimations')
-hold off
-set(gca,'Xscale','log')
-%% figure to show the convergance of the methods W-C
-figure;
-hold on; grid on;
-plot(SNR,theta_og*ones(size(SNR)),'-x','LineWidth', 2)
-plot(SNR,ThetaEst_MLE_white_colored,'-o'); plot(SNR,ThetaEst_MLE_colored_white,'-o');
-plot(SNR,ThetaEst_fisher_white_colored,'-*'); plot(SNR,ThetaEst_fisher_colored_white,'-*');
-legend('Original', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
-xlabel('SNR'); ylabel('\theta'); title('Comparison of the Estimations')
-hold off
-set(gca,'Xscale','log')
-%% figure to compare the MSPE to all kinds of CRB W-W, C-C
-figure;
-subplot(3,1,1)
-hold on; grid on;
-plot(SNR,CRB_white_reg,'-x','LineWidth', 2);plot(SNR,CRB_colored_reg,'-x','LineWidth', 2);
-plot(SNR,RMSPE_MLE_white_white,'-o'); plot(SNR,RMSPE_MLE_colored_colored,'-o');
-plot(SNR,RMSPE_fisher_white_white,'-*'); plot(SNR,RMSPE_fisher_colored_colored,'-*');
-legend('CRB W', 'CRB C', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
-xlabel('SNR'); ylabel('Regular MSPE'); title('regular CRB vs. MSPE of the Estimations')
-hold off
-set(gca,'Xscale','log')
-set(gca,'Yscale','log')
-
-
-
-subplot(3,1,2)
-hold on; grid on;
-plot(SNR,CRB_white_cyc1,'-x','LineWidth', 2);plot(SNR,CRB_colored_cyc2,'-x','LineWidth', 2);
-plot(SNR,RMSPE_MLE_white_white,'-o'); plot(SNR,RMSPE_MLE_colored_colored,'-o');
-plot(SNR,RMSPE_fisher_white_white,'-*'); plot(SNR,RMSPE_fisher_colored_colored,'-*');
-legend('CCRB_1 W', 'CCRB_1 C', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
-xlabel('SNR'); ylabel('Regular MSPE'); title('cyclic 1 CRB vs. MSPE of the Estimations')
-hold off
-set(gca,'Xscale','log')
-set(gca,'Yscale','log')
-
-
-subplot(3,1,3)
-hold on; grid on;
-plot(SNR,CRB_white_cyc2,'-x','LineWidth', 2);plot(SNR,CRB_colored_cyc2,'-x','LineWidth', 2);
-plot(SNR,RMSPE_MLE_white_white,'-o'); plot(SNR,RMSPE_MLE_colored_colored,'-o');
-plot(SNR,RMSPE_fisher_white_white,'-*'); plot(SNR,RMSPE_fisher_colored_colored,'-*');
-legend('CCRB_2 W', 'CCRB_2 C', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
-xlabel('SNR'); ylabel('Regular MSPE'); title('cyclic 2 CRB vs. MSPE of the Estimations')
-hold off
-set(gca,'Xscale','log')
-set(gca,'Yscale','log')
-%% figure to compare the MSPE to all kinds of CRB W-C
-figure;
-subplot(3,1,1)
-hold on; grid on;
-plot(SNR,CRB_white_reg,'-x','LineWidth', 2);plot(SNR,CRB_colored_reg,'-x','LineWidth', 2);
-plot(SNR,RMSPE_MLE_white_colored,'-o'); plot(SNR,RMSPE_MLE_colored_white,'-o');
-plot(SNR,RMSPE_fisher_white_colored,'-*'); plot(SNR,RMSPE_fisher_colored_white,'-*');
-legend('CRB W', 'CRB C', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
-xlabel('SNR'); ylabel('Regular MSPE'); title('regular CRB vs. MSPE of the Estimations')
-hold off
-set(gca,'Xscale','log')
-set(gca,'Yscale','log')
-
-
-
-subplot(3,1,2)
-hold on; grid on;
-plot(SNR,CRB_white_cyc1,'-x','LineWidth', 2);plot(SNR,CRB_colored_cyc2,'-x','LineWidth', 2);
-plot(SNR,RMSPE_MLE_white_colored,'-o'); plot(SNR,RMSPE_MLE_colored_white,'-o');
-plot(SNR,RMSPE_fisher_white_colored,'-*'); plot(SNR,RMSPE_fisher_colored_white,'-*');
-legend('CCRB_1 W', 'CCRB_1 C', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
-xlabel('SNR'); ylabel('Regular MSPE'); title('cyclic 1 CRB vs. MSPE of the Estimations')
-hold off
-set(gca,'Xscale','log')
-set(gca,'Yscale','log')
-
-
-subplot(3,1,3)
-hold on; grid on;
-plot(SNR,CRB_white_cyc2,'-x','LineWidth', 2);plot(SNR,CRB_colored_cyc2,'-x','LineWidth', 2);
-plot(SNR,RMSPE_MLE_white_colored,'-o'); plot(SNR,RMSPE_MLE_colored_white,'-o');
-plot(SNR,RMSPE_fisher_white_colored,'-*'); plot(SNR,RMSPE_fisher_colored_white,'-*');
-legend('CCRB_2 W', 'CCRB_2 C', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
-xlabel('SNR'); ylabel('Regular MSPE'); title('cyclic 2 CRB vs. MSPE of the Estimations')
-hold off
-set(gca,'Xscale','log')
-set(gca,'Yscale','log')
-
-%% figure to compare the cyclic MSPE to all kinds of CRB W-W, C-C
-figure;
-subplot(3,1,1)
-hold on; grid on;
-plot(SNR,CRB_white_reg,'-x','LineWidth', 2);plot(SNR,CRB_colored_reg,'-x','LineWidth', 2);
-plot(SNR,CyclicErr_MLE_white_white,'-o'); plot(SNR,CyclicErr_MLE_colored_colored,'-o');
-plot(SNR,CyclicErr_fisher_white_white,'-*'); plot(SNR,CyclicErr_fisher_colored_colored,'-*');
-legend('CRB W', 'CRB C', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
-xlabel('SNR'); ylabel('Cyclic Error'); title('Regular CRB vs. CMSPE of the Estimations')
-hold off;
-set(gca,'Xscale','log');set(gca,'Yscale','log')
-
-subplot(3,1,2)
-hold on; grid on;
-plot(SNR,CRB_white_cyc1,'-x','LineWidth', 2);plot(SNR,CRB_colored_cyc2,'-x','LineWidth', 2);
-plot(SNR,CyclicErr_MLE_white_white,'-o'); plot(SNR,CyclicErr_MLE_colored_colored,'-o');
-plot(SNR,CyclicErr_fisher_white_white,'-*'); plot(SNR,CyclicErr_fisher_colored_colored,'-*');
-legend('CRBC_1 W', 'CRBC_1 C', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
-xlabel('SNR'); ylabel('Cyclic Error'); title('cyclic 1 CRB vs. CMSPE of the Estimations')
-hold off
-set(gca,'Xscale','log');set(gca,'Yscale','log')
-
-subplot(3,1,3)
-hold on; grid on;
-plot(SNR,CRB_white_cyc2,'-x','LineWidth', 2);plot(SNR,CRB_colored_cyc2,'-x','LineWidth', 2);
-plot(SNR,CyclicErr_MLE_white_white,'-o'); plot(SNR,CyclicErr_MLE_colored_colored,'-o');
-plot(SNR,CyclicErr_fisher_white_white,'-*'); plot(SNR,CyclicErr_fisher_colored_colored,'-*');
-legend('CRBC_2 W', 'CRBC_2 C', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
-xlabel('SNR'); ylabel('Cyclic Error'); title('cyclic 2 CRB vs. CMSPE of the Estimations')
-hold off
-set(gca,'Xscale','log');set(gca,'Yscale','log')
-
-%% figure to compare the cyclic MSPE to all kinds of CRB W-C
-figure;
-subplot(3,1,1)
-hold on; grid on;
-plot(SNR,CRB_white_reg,'-x','LineWidth', 2);plot(SNR,CRB_colored_reg,'-x','LineWidth', 2);
-plot(SNR,CyclicErr_MLE_white_colored,'-o'); plot(SNR,CyclicErr_MLE_colored_white,'-o');
-plot(SNR,CyclicErr_fisher_white_colored,'-*'); plot(SNR,CyclicErr_fisher_colored_white,'-*');
-legend('CRB W', 'CRB C', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
-xlabel('SNR'); ylabel('Cyclic Error'); title('Regular CRB vs. CMSPE of the Estimations')
-hold off;
-set(gca,'Xscale','log');set(gca,'Yscale','log')
-
-subplot(3,1,2)
-hold on; grid on;
-plot(SNR,CRB_white_cyc1,'-x','LineWidth', 2);plot(SNR,CRB_colored_cyc2,'-x','LineWidth', 2);
-plot(SNR,CyclicErr_MLE_white_colored,'-o'); plot(SNR,CyclicErr_MLE_colored_white,'-o');
-plot(SNR,CyclicErr_fisher_white_colored,'-*'); plot(SNR,CyclicErr_fisher_colored_white,'-*');
-legend('CRBC_1 W', 'CRBC_1 C', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
-xlabel('SNR'); ylabel('Cyclic Error'); title('cyclic 1 CRB vs. CMSPE of the Estimations')
-hold off
-set(gca,'Xscale','log');set(gca,'Yscale','log')
-
-subplot(3,1,3)
-hold on; grid on;
-plot(SNR,CRB_white_cyc2,'-x','LineWidth', 2);plot(SNR,CRB_colored_cyc2,'-x','LineWidth', 2);
-plot(SNR,CyclicErr_MLE_white_colored,'-o'); plot(SNR,CyclicErr_MLE_colored_white,'-o');
-plot(SNR,CyclicErr_fisher_white_colored,'-*'); plot(SNR,CyclicErr_fisher_colored_white,'-*');
-legend('CRBC_2 W', 'CRBC_2 C', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
-xlabel('SNR'); ylabel('Cyclic Error'); title('cyclic 2 CRB vs. CMSPE of the Estimations')
-hold off
-set(gca,'Xscale','log');set(gca,'Yscale','log')
+res = dir('./res/ML_simulation_results_*.mat');
+save(append('./res/ML_simulation_results_', string(length(res)+1)));
+delete(gcp);
+clear all;
 
 %% Circle
 % angles = linspace(0, 2*pi, 500);
---
+++ prints.m
@@ -0,0 +1,168 @@
+
+close all;
+%load('./res/ML_simulation_results_i.mat'); %uncomment and replace i with requested results
+%% figure to show the convergance of the methods W-W, C-C
+figure;
+hold on; grid on;
+plot(SNR,theta_og*ones(size(SNR)),'LineWidth', 2)
+plot(SNR,ThetaEst_MLE_white_white); plot(SNR,ThetaEst_MLE_colored_colored);
+plot(SNR,ThetaEst_fisher_white_white); plot(SNR,ThetaEst_fisher_colored_colored);
+legend('Original', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
+xlabel('SNR'); ylabel('\theta'); title('Comparison of the Estimations')
+hold off
+set(gca,'Xscale','log')
+%% figure to show the convergance of the methods W-C
+figure;
+hold on; grid on;
+plot(SNR,theta_og*ones(size(SNR)),'LineWidth', 2)
+plot(SNR,ThetaEst_MLE_white_colored); plot(SNR,ThetaEst_MLE_colored_white);
+plot(SNR,ThetaEst_fisher_white_colored); plot(SNR,ThetaEst_fisher_colored_white);
+legend('Original', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
+xlabel('SNR'); ylabel('\theta'); title('Comparison of the Estimations')
+hold off
+set(gca,'Xscale','log')
+%% figure to compare the MSPE to all kinds of CRB W-W, C-C
+figure;
+subplot(3,1,1)
+hold on; grid on;
+plot(SNR,CRB_white_reg,'LineWidth', 2);plot(SNR,CRB_colored_reg,'LineWidth', 2);
+plot(SNR,RMSPE_MLE_white_white); plot(SNR,RMSPE_MLE_colored_colored);
+plot(SNR,RMSPE_fisher_white_white); plot(SNR,RMSPE_fisher_colored_colored);
+legend('CRB W', 'CRB C', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
+xlabel('SNR'); ylabel('Regular MSPE'); title('regular CRB vs. MSPE of the Estimations')
+hold off
+set(gca,'Xscale','log')
+set(gca,'Yscale','log')
+
+
+
+subplot(3,1,2)
+hold on; grid on;
+plot(SNR,CRB_white_cyc1,'LineWidth', 2);plot(SNR,CRB_colored_cyc2,'LineWidth', 2);
+plot(SNR,RMSPE_MLE_white_white); plot(SNR,RMSPE_MLE_colored_colored);
+plot(SNR,RMSPE_fisher_white_white); plot(SNR,RMSPE_fisher_colored_colored);
+legend('CCRB_1 W', 'CCRB_1 C', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
+xlabel('SNR'); ylabel('Regular MSPE'); title('cyclic 1 CRB vs. MSPE of the Estimations')
+hold off
+set(gca,'Xscale','log')
+set(gca,'Yscale','log')
+
+
+subplot(3,1,3)
+hold on; grid on;
+plot(SNR,CRB_white_cyc2,'LineWidth', 2);plot(SNR,CRB_colored_cyc2,'LineWidth', 2);
+plot(SNR,RMSPE_MLE_white_white); plot(SNR,RMSPE_MLE_colored_colored);
+plot(SNR,RMSPE_fisher_white_white); plot(SNR,RMSPE_fisher_colored_colored);
+legend('CCRB_2 W', 'CCRB_2 C', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
+xlabel('SNR'); ylabel('Regular MSPE'); title('cyclic 2 CRB vs. MSPE of the Estimations')
+hold off
+set(gca,'Xscale','log')
+set(gca,'Yscale','log')
+%% figure to compare the MSPE to all kinds of CRB W-C
+figure;
+subplot(3,1,1)
+hold on; grid on;
+plot(SNR,CRB_white_reg,'LineWidth', 2);plot(SNR,CRB_colored_reg,'LineWidth', 2);
+plot(SNR,RMSPE_MLE_white_colored); plot(SNR,RMSPE_MLE_colored_white);
+plot(SNR,RMSPE_fisher_white_colored); plot(SNR,RMSPE_fisher_colored_white);
+legend('CRB W', 'CRB C', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
+xlabel('SNR'); ylabel('Regular MSPE'); title('regular CRB vs. MSPE of the Estimations')
+hold off
+set(gca,'Xscale','log')
+set(gca,'Yscale','log')
+
+
+
+subplot(3,1,2)
+hold on; grid on;
+plot(SNR,CRB_white_cyc1,'LineWidth', 2);plot(SNR,CRB_colored_cyc2,'LineWidth', 2);
+plot(SNR,RMSPE_MLE_white_colored); plot(SNR,RMSPE_MLE_colored_white);
+plot(SNR,RMSPE_fisher_white_colored); plot(SNR,RMSPE_fisher_colored_white);
+legend('CCRB_1 W', 'CCRB_1 C', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
+xlabel('SNR'); ylabel('Regular MSPE'); title('cyclic 1 CRB vs. MSPE of the Estimations')
+hold off
+set(gca,'Xscale','log')
+set(gca,'Yscale','log')
+
+
+subplot(3,1,3)
+hold on; grid on;
+plot(SNR,CRB_white_cyc2,'LineWidth', 2);plot(SNR,CRB_colored_cyc2,'LineWidth', 2);
+plot(SNR,RMSPE_MLE_white_colored); plot(SNR,RMSPE_MLE_colored_white);
+plot(SNR,RMSPE_fisher_white_colored); plot(SNR,RMSPE_fisher_colored_white);
+legend('CCRB_2 W', 'CCRB_2 C', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
+xlabel('SNR'); ylabel('Regular MSPE'); title('cyclic 2 CRB vs. MSPE of the Estimations')
+hold off
+set(gca,'Xscale','log')
+set(gca,'Yscale','log')
+
+%% figure to compare the cyclic MSPE to all kinds of CRB W-W, C-C
+figure;
+subplot(3,1,1)
+hold on; grid on;
+plot(SNR,CRB_white_reg,'LineWidth', 2);plot(SNR,CRB_colored_reg,'LineWidth', 2);
+plot(SNR,CyclicErr_MLE_white_white); plot(SNR,CyclicErr_MLE_colored_colored);
+plot(SNR,CyclicErr_fisher_white_white); plot(SNR,CyclicErr_fisher_colored_colored);
+legend('CRB W', 'CRB C', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
+xlabel('SNR'); ylabel('Cyclic Error'); title('Regular CRB vs. CMSPE of the Estimations')
+hold off;
+set(gca,'Xscale','log');set(gca,'Yscale','log')
+
+subplot(3,1,2)
+hold on; grid on;
+plot(SNR,CRB_white_cyc1,'LineWidth', 2);plot(SNR,CRB_colored_cyc2,'LineWidth', 2);
+plot(SNR,CyclicErr_MLE_white_white); plot(SNR,CyclicErr_MLE_colored_colored);
+plot(SNR,CyclicErr_fisher_white_white); plot(SNR,CyclicErr_fisher_colored_colored);
+legend('CRBC_1 W', 'CRBC_1 C', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
+xlabel('SNR'); ylabel('Cyclic Error'); title('cyclic 1 CRB vs. CMSPE of the Estimations')
+hold off
+set(gca,'Xscale','log');set(gca,'Yscale','log')
+
+subplot(3,1,3)
+hold on; grid on;
+plot(SNR,CRB_white_cyc2,'LineWidth', 2);plot(SNR,CRB_colored_cyc2,'LineWidth', 2);
+plot(SNR,CyclicErr_MLE_white_white); plot(SNR,CyclicErr_MLE_colored_colored);
+plot(SNR,CyclicErr_fisher_white_white); plot(SNR,CyclicErr_fisher_colored_colored);
+legend('CRBC_2 W', 'CRBC_2 C', 'MLE W-W', 'MLE C-C', 'Fisher W-W', 'Fisher C-C')
+xlabel('SNR'); ylabel('Cyclic Error'); title('cyclic 2 CRB vs. CMSPE of the Estimations')
+hold off
+set(gca,'Xscale','log');set(gca,'Yscale','log')
+
+%% figure to compare the cyclic MSPE to all kinds of CRB W-C
+figure;
+subplot(3,1,1)
+hold on; grid on;
+plot(SNR,CRB_white_reg,'LineWidth', 2);plot(SNR,CRB_colored_reg,'LineWidth', 2);
+plot(SNR,CyclicErr_MLE_white_colored); plot(SNR,CyclicErr_MLE_colored_white);
+plot(SNR,CyclicErr_fisher_white_colored); plot(SNR,CyclicErr_fisher_colored_white);
+legend('CRB W', 'CRB C', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
+xlabel('SNR'); ylabel('Cyclic Error'); title('Regular CRB vs. CMSPE of the Estimations')
+hold off;
+set(gca,'Xscale','log');set(gca,'Yscale','log')
+
+subplot(3,1,2)
+hold on; grid on;
+plot(SNR,CRB_white_cyc1,'LineWidth', 2);plot(SNR,CRB_colored_cyc2,'LineWidth', 2);
+plot(SNR,CyclicErr_MLE_white_colored); plot(SNR,CyclicErr_MLE_colored_white);
+plot(SNR,CyclicErr_fisher_white_colored); plot(SNR,CyclicErr_fisher_colored_white);
+legend('CRBC_1 W', 'CRBC_1 C', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
+xlabel('SNR'); ylabel('Cyclic Error'); title('cyclic 1 CRB vs. CMSPE of the Estimations')
+hold off
+set(gca,'Xscale','log');set(gca,'Yscale','log')
+
+subplot(3,1,3)
+hold on; grid on;
+plot(SNR,CRB_white_cyc2,'LineWidth', 2);plot(SNR,CRB_colored_cyc2,'LineWidth', 2);
+plot(SNR,CyclicErr_MLE_white_colored); plot(SNR,CyclicErr_MLE_colored_white);
+plot(SNR,CyclicErr_fisher_white_colored); plot(SNR,CyclicErr_fisher_colored_white);
+legend('CRBC_2 W', 'CRBC_2 C', 'MLE W-C', 'MLE C-W', 'Fisher W-C', 'Fisher C-W')
+xlabel('SNR'); ylabel('Cyclic Error'); title('cyclic 2 CRB vs. CMSPE of the Estimations')
+hold off
+set(gca,'Xscale','log');set(gca,'Yscale','log')
+
+if (save_pics)
+for i=1:6
+    name = num2str(i);
+    saveas(figure(i),append('./pics/',name),'png')
+end
+end
