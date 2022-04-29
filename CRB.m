0cb91d3f2a78665d9de02289b7c946f1a7fa5e66
Elias Assaf <elyas51000@gmail.com>
2022-04-29 10:56:56 +0300

    add comments

--- CRB.m
+++ CRB.m
@@ -1,6 +1,20 @@
-function [crb] = CRB(type,v_0,alpha,theta,Rv,M,X,a_f,da_f)
+function [crb] = CRB(type,v_0,alpha,theta,Rv,M,X,a_f,da_f,P)
 %This function calculates the CRB
 % depending on the type of the bound ('cyclic 1', 'cyclic 2','regular')
+% v_0 is the speed of the wave
+% alpha is the incidence angle
+% theta is the doa angle
+% Rv is the correlation matrix, note that we take the pseudo inverse of it,
+% since our correlation matrix is non-singular
+% M the number of frequencies in the signal
+% X is the signal in frequency, which has P segments and each segment
+% contains M samples
+% a_f is the first function returned from model.m (the steering vector)
+% da_f is the second function returned from model.m (derivative of the
+% steering vector)
+% P the number of segments in the signal, unused
+
+%returns the value of the CRB bound
 Rv_inv = pinv(Rv);
 X_w = squeeze(sum(X,1));
 A = cell2mat(arrayfun(a_f,1 : M,theta*ones(1,M), alpha*ones(1,M), v_0*ones(1,M),'uniformoutput',false));
