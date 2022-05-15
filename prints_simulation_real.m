d01fc3356ec5d7be508d51c6a5a6aac70b7a1aed
Elias Assaf <elyas51000@gmail.com>
2022-05-15 15:06:10 +0300

    print all res

--- prints_simulation_real.m
+++ prints_simulation_real.m
@@ -6,18 +6,18 @@
 % Instructions: load the output file of simulation then run
 % this function.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-load('./res/beamformer_simulation_real_results_2.mat'); %uncomment and replace i with requested results
+load('./res/BEAMFORMER_simulation_real_white_results_3.mat'); %uncomment and replace i with requested results
 save_pics = false;
 samples_for_mean = 30;
 fig2 = figure;sgtitle("error histrograms");
-subplot(3,2,1);histogram(estimated_error_cyclic);title("cyclic beamformer");
+subplot(4,2,1);histogram(estimated_error_cyclic);title("cyclic beamformer");
 xlabel("bin");ylabel("error value")
-subplot(3,2,2);histogram(estimated_error_MSPE);title("MSPE beamformer");
+subplot(4,2,2);histogram(estimated_error_MSPE);title("MSPE beamformer");
 xlabel("bin");ylabel("error value")
 
 fig = figure;sgtitle("errors")
 %% figure to errors
-subplot(1,3,1);title('beamformer');
+subplot(1,4,1);title('beamformer');
 cyclic = mean(reshape(estimated_error_cyclic(1:length(estimated_error_cyclic)-mod(length(estimated_error_cyclic),samples_for_mean)), samples_for_mean, []), 1);
 MSPE = mean(reshape(estimated_error_MSPE(1:length(estimated_error_cyclic)-mod(length(estimated_error_cyclic),samples_for_mean)), samples_for_mean, []), 1);
 real_err = mean(reshape(real_errors(1:length(real_errors)-mod(length(estimated_error_cyclic),samples_for_mean)), samples_for_mean, []), 1);
@@ -25,15 +25,15 @@ hold on;plot(cyclic);plot(MSPE);plot(real_err);
 xlabel('# of sample');ylabel('mean error');
 legend('cyclic', 'MSPE', 'real');
 
-load('./res/ML_simulation_real_white_results_1.mat');
+load('./res/MLE_WHITE_simulation_real_white_results_2.mat');
 figure(fig2);
-subplot(3,2,3);histogram(estimated_error_cyclic);title("cyclic ML");
+subplot(4,2,3);histogram(estimated_error_cyclic);title("cyclic ML");
 xlabel("bin");ylabel("error value")
-subplot(3,2,4);histogram(estimated_error_MSPE);title("MSPE ML");
+subplot(4,2,4);histogram(estimated_error_MSPE);title("MSPE ML");
 xlabel("bin");ylabel("error value")
 
 figure(fig);
-subplot(1,3,2);title('ML real data assuming white noise');
+subplot(1,4,2);title('ML real data assuming white noise');
 cyclic = mean(reshape(estimated_error_cyclic(1:length(estimated_error_cyclic)-mod(length(estimated_error_cyclic),samples_for_mean)), samples_for_mean, []), 1);
 MSPE = mean(reshape(estimated_error_MSPE(1:length(estimated_error_cyclic)-mod(length(estimated_error_cyclic),samples_for_mean)), samples_for_mean, []), 1);
 real_err = mean(reshape(real_errors(1:length(real_errors)-mod(length(estimated_error_cyclic),samples_for_mean)), samples_for_mean, []), 1);
@@ -41,15 +41,31 @@ hold on;plot(cyclic);plot(MSPE);plot(real_err);
 xlabel('# of sample');ylabel('mean error');
 legend('cyclic', 'MSPE', 'real');
 
-load('./res/Fisher_scoring_simulation_real_white_1.mat');
+load('./res/FISHER_SCORING_simulation_real_white_results_2.mat');
 figure(fig2);
-subplot(3,2,5);histogram(estimated_error_cyclic);title("cyclic fisher scoring");
+subplot(4,2,5);histogram(estimated_error_cyclic);title("cyclic fisher scoring");
 xlabel("bin");ylabel("error value")
-subplot(3,2,6);histogram(estimated_error_MSPE);title("MSPE fisher scoring");
+subplot(4,2,6);histogram(estimated_error_MSPE);title("MSPE fisher scoring");
 xlabel("bin");ylabel("error value")
 
 figure(fig);
-subplot(1,3,3);title('Fisher scoring real data assuming white noise');
+subplot(1,4,3);title('Fisher scoring real data assuming white noise');
+cyclic = mean(reshape(estimated_error_cyclic(1:length(estimated_error_cyclic)-mod(length(estimated_error_cyclic),samples_for_mean)), samples_for_mean, []), 1);
+MSPE = mean(reshape(estimated_error_MSPE(1:length(estimated_error_cyclic)-mod(length(estimated_error_cyclic),samples_for_mean)), samples_for_mean, []), 1);
+real_err = mean(reshape(real_errors(1:length(real_errors)-mod(length(estimated_error_cyclic),samples_for_mean)), samples_for_mean, []), 1);
+hold on;plot(cyclic);plot(MSPE);plot(real_err);
+xlabel('# of sample');ylabel('mean error');
+legend('cyclic', 'MSPE', 'real');
+
+load('./res/FISHER_SCORING_ORIG_simulation_real_white_results_1.mat');
+figure(fig2);
+subplot(4,2,7);histogram(estimated_error_cyclic);title("cyclic orig fisher scoring");
+xlabel("bin");ylabel("error value")
+subplot(4,2,8);histogram(estimated_error_MSPE);title("MSPE orig fisher scoring");
+xlabel("bin");ylabel("error value")
+
+figure(fig);
+subplot(1,4,4);title('Orig Fisher scoring real data assuming white noise');
 cyclic = mean(reshape(estimated_error_cyclic(1:length(estimated_error_cyclic)-mod(length(estimated_error_cyclic),samples_for_mean)), samples_for_mean, []), 1);
 MSPE = mean(reshape(estimated_error_MSPE(1:length(estimated_error_cyclic)-mod(length(estimated_error_cyclic),samples_for_mean)), samples_for_mean, []), 1);
 real_err = mean(reshape(real_errors(1:length(real_errors)-mod(length(estimated_error_cyclic),samples_for_mean)), samples_for_mean, []), 1);
